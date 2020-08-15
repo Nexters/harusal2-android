6e938dd9f1b9d664b3a82b4dab412c817f5b239d
KM <sodp2@naver.com>
2020-08-15 19:03:27 +0900

    feat: #10 아이콘 적용
    - background 색상 설정하는 함수 추가
    - 메뉴, 리스트 아이콘 추가

--- app/src/main/java/com/nexters/zzallang/harusal2/ui/main/MainActivity.kt
+++ app/src/main/java/com/nexters/zzallang/harusal2/ui/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.nexters.zzallang.harusal2.ui.main
 
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.annotation.ColorRes
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.airbnb.lottie.LottieDrawable
 import com.nexters.zzallang.harusal2.R
@@ -11,11 +13,10 @@ import com.nexters.zzallang.harusal2.ui.main.decoration.MainStatementDecoration
 import com.nexters.zzallang.harusal2.ui.main.model.MainStatement
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
+
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun layoutRes(): Int = R.layout.activity_main
     override val viewModel: MainViewModel by viewModel()
-
-    private val LOTTIE_INFINITE_COUNT: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +27,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun bindingView() {
         super.bindingView()
 
+        setBackgroundColor(R.color.test)
+
         binding.ivEmoji.playAnimation()
-        binding.ivEmoji.repeatCount = LOTTIE_INFINITE_COUNT
+        binding.ivEmoji.repeatCount = LottieDrawable.INFINITE
 
         binding.rcvStatement.layoutManager = LinearLayoutManager(this)
         binding.rcvStatement.addItemDecoration(MainStatementDecoration())
@@ -55,5 +58,14 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             ))
         }
         binding.rcvStatement.setHasFixedSize(true)
+    }
+
+    private fun setBackgroundColor(@ColorRes resId: Int) {
+        val color = resources.getColor(resId, null)
+        binding.layoutWhole.setBackgroundColor(color)
+        binding.layoutAppbar.setBackgroundColor(color)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = color
     }
 }
