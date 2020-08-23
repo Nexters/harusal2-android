1ef8c8775e8b8fc5daa7c47b38cf478e98d7a833
Taehyun <ddd2520@naver.com>
2020-08-23 16:10:23 +0900

    fix: #32 budget entity 생성

--- app/src/main/java/com/nexters/zzallang/harusal2/application/di/UseCaseModule.kt
+++ app/src/main/java/com/nexters/zzallang/harusal2/application/di/UseCaseModule.kt
@@ -1,10 +1,11 @@
 package com.nexters.zzallang.harusal2.application.di
 
 import com.nexters.zzallang.harusal2.usecase.BudgetUseCase
+import com.nexters.zzallang.harusal2.usecase.PeriodUseCase
 import com.nexters.zzallang.harusal2.usecase.StatementUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
     single { StatementUseCase(get()) }
-    single { BudgetUseCase() }
+    single { BudgetUseCase(get()) }
 }
---
+++ app/src/main/java/com/nexters/zzallang/harusal2/usecase/PeriodUseCase.kt
@@ -0,0 +1,8 @@
+package com.nexters.zzallang.harusal2.usecase
+
+import com.nexters.zzallang.harusal2.data.entity.Budget
+import com.nexters.zzallang.harusal2.data.repository.BudgetRepository
+
+class PeriodUseCase() {
+
+}
