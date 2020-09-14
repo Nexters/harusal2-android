31e5c230aa325cf99ae2f4e85946cbd924df5638
KM <sodp2@naver.com>
2020-09-15 01:29:13 +0900

    feat: #29 환경설정 UI 생성

---
+++ app/src/main/java/com/nexters/zzallang/harusal2/ui/setting/SettingActivity.kt
@@ -0,0 +1,18 @@
+package com.nexters.zzallang.harusal2.ui.setting
+
+import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import com.nexters.zzallang.harusal2.R
+import com.nexters.zzallang.harusal2.base.BaseActivity
+import com.nexters.zzallang.harusal2.databinding.ActivitySettingBinding
+
+class SettingActivity: BaseActivity<ActivitySettingBinding>() {
+    override val viewModel: ViewModel
+        get() = TODO("Not yet implemented")
+
+    override fun layoutRes(): Int = R.layout.activity_setting
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
