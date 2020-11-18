fa0d9cbd9b8f25630e6ee9d4c8d5391f6921b157
Taehyun <ddd2520@naver.com>
2020-11-18 21:03:09 +0900

    fix: #97 메인화면 금액 오류 수정

--- app/src/main/java/com/nexters/zzallang/harusal2/ui/main/MainViewModel.kt
+++ app/src/main/java/com/nexters/zzallang/harusal2/ui/main/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import com.nexters.zzallang.harusal2.application.util.DateUtils
 import com.nexters.zzallang.harusal2.base.BaseViewModel
 import com.nexters.zzallang.harusal2.data.entity.Budget
+import com.nexters.zzallang.harusal2.data.entity.Statement
 import com.nexters.zzallang.harusal2.ui.main.model.MainStatement
 import com.nexters.zzallang.harusal2.usecase.BudgetUseCase
 import com.nexters.zzallang.harusal2.usecase.StatementUseCase
@@ -20,17 +21,15 @@ class MainViewModel(
     val todayRemainMoney: LiveData<String> get() = _todayRemainMoney
     private val _todayLivingExpenses = MutableLiveData<String>("오늘의 생활비 0원")
     val todayLivingExpenses: LiveData<String> get() = _todayLivingExpenses
-    lateinit var budget :Budget
+    private lateinit var budget: Budget
+    private lateinit var statements: List<Statement>
+    private lateinit var todayStatements: List<Statement>
     private var remainMoney: Int = 0
     private var livingExpenses: Int = 0
 
-    private suspend fun refreshTodaySpendMoney() {
-        val list = withContext(Dispatchers.IO + job) {
-            statementUseCase.findStatementByBudgetId(budget.id)
-        }.filter { statement -> statement.date.date == Date().date }
-
+    private fun refreshTodaySpendMoney() {
         var tempMoney = livingExpenses
-        for (item in list) {
+        for (item in todayStatements) {
             tempMoney += item.amount
         }
 
@@ -38,13 +37,10 @@ class MainViewModel(
         _todayRemainMoney.postValue("${remainMoney}원")
     }
 
-    suspend fun getTodaySpendStatementList(): List<MainStatement> {
-        val list = withContext(Dispatchers.IO + job) {
-            statementUseCase.findStatementByBudgetId(budget.id)
-        }.filter { statement -> statement.date.date == Date().date }
+    fun getTodaySpendStatementList(): List<MainStatement> {
         val result = arrayListOf<MainStatement>()
 
-        for (item in list) {
+        for (item in todayStatements) {
             if (item.content.length > 10) {
                 item.content = item.content.substring(0..10).plus("...")
             }
@@ -54,27 +50,39 @@ class MainViewModel(
         return result
     }
 
-    private suspend fun refreshTodayLivingExpenses() {
-        budget = withContext(Dispatchers.IO + job) {
-            budgetUseCase.findRecentBudget()
-        }
+    private fun refreshTodayLivingExpenses() {
+        val yesterdayMillis = 1000 * 60 * 60 * 24
+        var spentMoney = 0
+        var yesterdayDate = Date(Date().time - yesterdayMillis)
+        yesterdayDate.hours = 23
+        yesterdayDate.minutes = 59
+        yesterdayDate.seconds = 59
+
+        statements.filter { statement -> statement.date.before(yesterdayDate) }
+            .forEach { statement: Statement -> spentMoney += statement.amount }
 
         val remainDate = DateUtils.calculateDate(Date(), budget.endDate)
 
-        livingExpenses = budget.budget / remainDate
+        livingExpenses = (budget.budget + spentMoney) / remainDate
         _todayLivingExpenses.postValue("오늘의 생활비 ${livingExpenses}원")
     }
 
     suspend fun checkCurrentSpendState(): SpendState {
-        var statementListSize: Int = 0
-
-        withContext(Dispatchers.IO + job) {
-            refreshTodayLivingExpenses()
-            refreshTodaySpendMoney()
-            statementListSize = getTodaySpendStatementList().size
+        budget = withContext(Dispatchers.IO + job) {
+            budgetUseCase.findRecentBudget()
         }
 
-        return if (statementListSize == 0) {
+        statements = withContext(Dispatchers.IO + job) {
+            statementUseCase.findStatementByBudgetId(budget.id)
+        }
+
+        todayStatements = statements.filter { statement -> statement.date.date == Date().date }
+
+        refreshTodayLivingExpenses()
+        refreshTodaySpendMoney()
+        val todayStatementSize = getTodaySpendStatementList().size
+
+        return if (todayStatementSize == 0) {
             SpendState.DEFAULT
         } else if ((livingExpenses * (7f / 10)) < remainMoney) {
             SpendState.FLEX
