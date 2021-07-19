738c1ea222b6580fb75ea4a9f9271e2d4b2a4ecc
KM <sodp2@naver.com>
2021-07-19 22:36:55 +0900

    chore: #111 targetSDK 30으로 변경
    - compileSdkVersion, buildToolsVersion도 변경
    - null 처리 추가

--- app/src/main/java/com/nexters/zzallang/harusal2/ui/budget/register/StartDayDefaultRegisterActivity.kt
+++ app/src/main/java/com/nexters/zzallang/harusal2/ui/budget/register/StartDayDefaultRegisterActivity.kt
@@ -2,6 +2,7 @@ package com.nexters.zzallang.harusal2.ui.budget.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import com.nexters.zzallang.harusal2.R
 import com.nexters.zzallang.harusal2.application.util.IntentUtils
 import com.nexters.zzallang.harusal2.base.BaseActivity
@@ -36,9 +37,15 @@ class StartDayDefaultRegisterActivity : BaseActivity<ActivityRegisterDayDefaultB
         }
 
         binding.btnComplete.setOnClickListener {
-            viewModel.saveBudgetDay(this.intent.getStringExtra("budget").toInt())
-            this.startActivity(IntentUtils.getMainActivityIntent(this))
-            this.finish()
+            val budget = this.intent.getStringExtra("budget")
+            if (budget != null) {
+                viewModel.saveBudgetDay(budget.toInt())
+
+                this.startActivity(IntentUtils.getMainActivityIntent(this))
+                this.finish()
+            } else {
+                Toast.makeText(this, "정상적으로 저장되지 않았습니다. 다시 실행해주세요.", Toast.LENGTH_SHORT).show()
+            }
         }
 
         binding.btnClose.setOnClickListener {
