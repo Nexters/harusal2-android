14cb112e44be36b4790e34c04126091dd8d8c5d1
gowoonchoi <gowoonvv@gmail.com>
2021-08-31 11:12:36 +0900

    feat : add open app and refresh click event

--- app/src/main/java/com/nexters/zzallang/harusal2/ui/appWidget/WidgetProviderType1.kt
+++ app/src/main/java/com/nexters/zzallang/harusal2/ui/appWidget/WidgetProviderType1.kt
@@ -1,10 +1,13 @@
 package com.nexters.zzallang.harusal2.ui.appWidget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
 import com.nexters.zzallang.harusal2.R
+import com.nexters.zzallang.harusal2.ui.splash.SplashActivity
 
 class WidgetProviderType1 : AppWidgetProvider() {
     override fun onUpdate(
@@ -12,14 +15,44 @@ class WidgetProviderType1 : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager?,
         appWidgetIds: IntArray?
     ) {
-        appWidgetIds?.forEach { appWidgetIds ->
+        appWidgetIds?.forEach { appWidgetId ->
                 RemoteViews(context?.packageName, R.layout.widget_type1).also {
 
-
-                    appWidgetManager?.updateAppWidget(appWidgetIds, it)
+                    it.setOnClickOpenApp(context, appWidgetId)
+                    it.setOnClickRefresh(context, appWidgetId)
+                    appWidgetManager?.updateAppWidget(appWidgetId, it)
                 }
         }
     }
 
+    private fun RemoteViews.setOnClickOpenApp(context : Context?, appWidgetId : Int){
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            APP_WIDGET_REQUEST_CODE + appWidgetId,
+            Intent(context, SplashActivity::class.java),
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
 
+        setOnClickPendingIntent(R.id.container, pendingIntent)
+    }
+
+    private fun RemoteViews.setOnClickRefresh(context : Context?, appWidgetId : Int){
+        val intent = Intent(context, WidgetProviderType1::class.java).apply {
+            action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
+            putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, intArrayOf(appWidgetId))
+        }
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            APP_WIDGET_REQUEST_CODE + appWidgetId,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        setOnClickPendingIntent(R.id.iv_refresh, pendingIntent)
+    }
+
+    companion object{
+        const val APP_WIDGET_REQUEST_CODE = 100
+    }
 }
