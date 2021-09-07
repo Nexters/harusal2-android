89b4f02c5766216647bc61a6f3962263c8c0aecd
YChaeeun <ychae.leah@gmail.com>
2021-09-08 00:02:51 +0900

    feature: 위젯 type 3 usecase 적용, color 변경 적용

--- app/src/main/java/com/nexters/zzallang/harusal2/ui/appwidget/TypeThreeWidgetProvider.kt
+++ app/src/main/java/com/nexters/zzallang/harusal2/ui/appwidget/TypeThreeWidgetProvider.kt
@@ -7,27 +7,30 @@ import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
 import com.nexters.zzallang.harusal2.R
-import com.nexters.zzallang.harusal2.application.util.DateUtils
 import com.nexters.zzallang.harusal2.application.util.NumberUtils
+import com.nexters.zzallang.harusal2.constant.SpendState
 import com.nexters.zzallang.harusal2.data.repository.BudgetRepository
 import com.nexters.zzallang.harusal2.data.repository.StatementRepository
 import com.nexters.zzallang.harusal2.ui.splash.SplashActivity
-import com.nexters.zzallang.harusal2.usecase.BudgetUseCase
-import com.nexters.zzallang.harusal2.usecase.StatementUseCase
-import kotlinx.coroutines.*
-import java.time.LocalDate
+import com.nexters.zzallang.harusal2.usecase.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-class RemainedMoneyWidgetProvider : AppWidgetProvider() {
+class TypeThreeWidgetProvider : AppWidgetProvider() {
 	companion object {
 		const val APP_WIDGET_REQUEST_CODE = 100
 	}
 
-	private val job = Job()
-	private val statementUseCase = StatementUseCase(StatementRepository())
-	private val budgetUseCase = BudgetUseCase(BudgetRepository())
+	private val budgetRepository = BudgetRepository()
+	private val statementRepository = StatementRepository()
 
-	private var livingExpenses = 0
-	private var remainMoney = 0
+	private val getTodayBudgetUseCase = GetTodayBudgetUseCase(budgetRepository, statementRepository)
+	private val getRemainMoneyUseCase = GetRemainMoneyUseCase(budgetRepository, statementRepository)
+	private val getSpentMoneyStatusUseCase = GetSpentMoneyStatusUseCase()
+
+	private val job = Job()
 
 	override fun onReceive(context: Context?, intent: Intent?) {
 		super.onReceive(context, intent)
@@ -39,9 +42,10 @@ class RemainedMoneyWidgetProvider : AppWidgetProvider() {
 		CoroutineScope(Dispatchers.Main + job).launch {
 			appWidgetIds?.forEach { appWidgetId ->
 				RemoteViews(context?.packageName, R.layout.appwidget_type_3).also {
-					initData()
+					val todayBudget = getTodayBudgetUseCase.getTodayBudget()
+					val remainMoney = getRemainMoneyUseCase.getRemainMoney(todayBudget)
 
-					it.setImageViewResource(R.id.status, getStatus())
+					it.setImageViewResource(R.id.status, getStatus(todayBudget, remainMoney))
 					it.setTextViewText(R.id.tv_remain_money, NumberUtils.decimalFormat.format(remainMoney))
 					it.setOnClickRefresh(context, appWidgetId)
 					it.setOnClickOpenApp(context, appWidgetId)
@@ -64,7 +68,7 @@ class RemainedMoneyWidgetProvider : AppWidgetProvider() {
 	}
 
 	private fun RemoteViews.setOnClickRefresh(context: Context?, appWidgetId: Int) {
-		val intent = Intent(context, RemainedMoneyWidgetProvider::class.java).apply {
+		val intent = Intent(context, TypeThreeWidgetProvider::class.java).apply {
 			action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
 			putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, intArrayOf(appWidgetId))
 		}
@@ -79,51 +83,14 @@ class RemainedMoneyWidgetProvider : AppWidgetProvider() {
 		setOnClickPendingIntent(R.id.refresh, pendingIntent)
 	}
 
-	private fun getStatus() = when {
-		(livingExpenses * (7f / 10)) < remainMoney -> {
-			R.drawable.appwidget_status_blue
+	private fun getStatus(todayBudget: Int, remainMoney: Int): Int {
+		return when (getSpentMoneyStatusUseCase.getSpentMoneyStatus(todayBudget, remainMoney)) {
+			SpendState.FLEX -> R.drawable.appwidget_status_blue
+			SpendState.CLAP -> R.drawable.appwidget_status_mint
+			SpendState.EMBARRASSED -> R.drawable.appwidget_status_yellow
+			SpendState.CRY -> R.drawable.appwidget_status_orange
+			else -> R.drawable.appwidget_status_red
 		}
-		(livingExpenses * (1f / 10)) < remainMoney -> {
-			R.drawable.appwidget_status_green
-		}
-		(livingExpenses * (4f / 10)) < remainMoney -> {
-			R.drawable.appwidget_status_yellow
-		}
-		(livingExpenses * (8f / 10)) < remainMoney -> {
-			R.drawable.appwidget_status_orange
-		}
-		else -> {
-			R.drawable.appwidget_status_red
-		}
-	}
-
-	private suspend fun initData() {
-		var spentMoney = 0
-		val now = LocalDate.now()
-
-		val budget = withContext(CoroutineScope(Dispatchers.IO + job).coroutineContext) {
-			budgetUseCase.findRecentBudget()
-		}
-
-		val statements = withContext(CoroutineScope(Dispatchers.IO + job).coroutineContext) {
-			statementUseCase.findStatementByBudgetId(budget.id)
-		}
-
-		statements.filter { it.date.isBefore(now) }.forEach { spentMoney += it.amount }
-
-		val remainDate = DateUtils.calculateDate(now, budget.endDate)
-
-		livingExpenses = (budget.budget + spentMoney) / remainDate
-
-		val todayStatements =
-			statements.filter { statement -> statement.date.dayOfMonth == LocalDate.now().dayOfMonth }
-
-		var tempMoney = livingExpenses
-		for (item in todayStatements) {
-			tempMoney += item.amount
-		}
-
-		remainMoney = tempMoney
 	}
 
 	override fun onDeleted(context: Context?, appWidgetIds: IntArray?) {
