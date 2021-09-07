55435c9f449c9b9ec918082fd92276ea19cdd164
YChaeeun <ychae.leah@gmail.com>
2021-09-08 00:44:35 +0900

    feature: 위젯 type 5 구현

--- app/src/main/java/com/nexters/zzallang/harusal2/ui/appwidget/TypeFiveWidgetProvider.kt
+++ app/src/main/java/com/nexters/zzallang/harusal2/ui/appwidget/TypeFiveWidgetProvider.kt
@@ -8,27 +8,29 @@ import android.content.Intent
 import android.widget.RemoteViews
 import com.nexters.zzallang.harusal2.R
 import com.nexters.zzallang.harusal2.application.util.NumberUtils
-import com.nexters.zzallang.harusal2.constant.SpendState
 import com.nexters.zzallang.harusal2.data.repository.BudgetRepository
 import com.nexters.zzallang.harusal2.data.repository.StatementRepository
 import com.nexters.zzallang.harusal2.ui.splash.SplashActivity
-import com.nexters.zzallang.harusal2.usecase.*
+import com.nexters.zzallang.harusal2.usecase.GetRemainDayUseCase
+import com.nexters.zzallang.harusal2.usecase.GetRemainMoneyUseCase
+import com.nexters.zzallang.harusal2.usecase.GetTodayBudgetUseCase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-class TypeThreeWidgetProvider : AppWidgetProvider() {
+class TypeFiveWidgetProvider : AppWidgetProvider() {
 	companion object {
-		const val APP_WIDGET_REQUEST_CODE = 100
+		const val FIVE_REQUEST_CODE = 101
 	}
 
 	private val budgetRepository = BudgetRepository()
 	private val statementRepository = StatementRepository()
 
+	private val getRemainDayUseCase = GetRemainDayUseCase(budgetRepository)
+
 	private val getTodayBudgetUseCase = GetTodayBudgetUseCase(budgetRepository, statementRepository)
 	private val getRemainMoneyUseCase = GetRemainMoneyUseCase(budgetRepository, statementRepository)
-	private val getSpentMoneyStatusUseCase = GetSpentMoneyStatusUseCase()
 
 	private val job = Job()
 
@@ -41,13 +43,14 @@ class TypeThreeWidgetProvider : AppWidgetProvider() {
 
 		CoroutineScope(Dispatchers.Main + job).launch {
 			appWidgetIds?.forEach { appWidgetId ->
-				RemoteViews(context?.packageName, R.layout.appwidget_type_three_layout).also {
+				RemoteViews(context?.packageName, R.layout.appwidget_type_five_layout).also {
 					val todayBudget = getTodayBudgetUseCase.getTodayBudget()
 					val remainMoney = getRemainMoneyUseCase.getRemainMoney(todayBudget)
 
+					val remainDayText = context?.resources?.getString(R.string.appwidget_d_day, getRemainDayUseCase.getRemainDay()).orEmpty()
 					val remainMoneyText = context?.resources?.getString(R.string.appwidget_remaining_money, NumberUtils.decimalFormat.format(remainMoney)).orEmpty()
 
-					it.setImageViewResource(R.id.status, getStatus(todayBudget, remainMoney))
+					it.setTextViewText(R.id.d_day, remainDayText)
 					it.setTextViewText(R.id.tv_remain_money, remainMoneyText)
 
 					it.setOnClickRefresh(context, appWidgetId)
@@ -62,7 +65,7 @@ class TypeThreeWidgetProvider : AppWidgetProvider() {
 	private fun RemoteViews.setOnClickOpenApp(context: Context?, appWidgetId: Int) {
 		val pendingIntent = PendingIntent.getActivity(
 			context,
-			APP_WIDGET_REQUEST_CODE + appWidgetId,
+			FIVE_REQUEST_CODE + appWidgetId,
 			Intent(context, SplashActivity::class.java),
 			PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
 		)
@@ -71,29 +74,19 @@ class TypeThreeWidgetProvider : AppWidgetProvider() {
 	}
 
 	private fun RemoteViews.setOnClickRefresh(context: Context?, appWidgetId: Int) {
-		val intent = Intent(context, TypeThreeWidgetProvider::class.java).apply {
+		val intent = Intent(context, TypeFiveWidgetProvider::class.java).apply {
 			action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
 			putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, intArrayOf(appWidgetId))
 		}
 
 		val pendingIntent = PendingIntent.getBroadcast(
 			context,
-			APP_WIDGET_REQUEST_CODE + appWidgetId,
+			FIVE_REQUEST_CODE + appWidgetId,
 			intent,
 			PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
 		)
 
 		setOnClickPendingIntent(R.id.refresh, pendingIntent)
-	}
-
-	private fun getStatus(todayBudget: Int, remainMoney: Int): Int {
-		return when (getSpentMoneyStatusUseCase.getSpentMoneyStatus(todayBudget, remainMoney)) {
-			SpendState.FLEX -> R.drawable.appwidget_status_blue
-			SpendState.CLAP -> R.drawable.appwidget_status_mint
-			SpendState.EMBARRASSED -> R.drawable.appwidget_status_yellow
-			SpendState.CRY -> R.drawable.appwidget_status_orange
-			else -> R.drawable.appwidget_status_red
-		}
 	}
 
 	override fun onDeleted(context: Context?, appWidgetIds: IntArray?) {
