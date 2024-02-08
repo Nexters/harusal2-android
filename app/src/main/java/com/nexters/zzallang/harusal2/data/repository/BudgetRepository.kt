9fc7ad906a53abc15214a1f19890410f746b3be6
AnKyungMoo <sodp2@naver.com>
2024-02-08 20:35:38 +0900

    refactor: #139 BaseRepository 제거

--- app/src/main/java/com/nexters/zzallang/harusal2/data/repository/BudgetRepository.kt
+++ app/src/main/java/com/nexters/zzallang/harusal2/data/repository/BudgetRepository.kt
@@ -1,41 +1,34 @@
 package com.nexters.zzallang.harusal2.data.repository
 
 import com.nexters.zzallang.harusal2.application.AppDatabase
-import com.nexters.zzallang.harusal2.base.BaseRepository
 import com.nexters.zzallang.harusal2.data.entity.Budget
-import kotlinx.coroutines.withContext
 
-class BudgetRepository : BaseRepository() {
-    suspend fun insertBudget(budget: Budget) =
-        withContext(coroutineContext) {
-            AppDatabase.instance.budgetDao().insertBudget(budget)
-        }
-
-    suspend fun findById(id: Long) =
-        withContext(coroutineContext) {
-            AppDatabase.instance.budgetDao().findById(id)
-        }
-
-    suspend fun findRecentBudget() =
-        withContext(coroutineContext) {
-            AppDatabase.instance.budgetDao().findRecentBudget()
-        }
-
-    suspend fun update(budget: Budget) =
-        withContext(coroutineContext) {
-            AppDatabase.instance.budgetDao().update(budget)
-        }
-
-    suspend fun findAll() =
-        withContext(coroutineContext) {
-            AppDatabase.instance.budgetDao().findAll()
-        }
-
-    suspend fun findAllDesc() = withContext(coroutineContext) {
-        AppDatabase.instance.budgetDao().findAllDesc()
+class BudgetRepository {
+    suspend fun insertBudget(budget: Budget) {
+        AppDatabase.instance.budgetDao().insertBudget(budget)
     }
 
-    suspend fun deleteAllBudget() = withContext(coroutineContext) {
+    suspend fun findById(id: Long): Budget {
+        return AppDatabase.instance.budgetDao().findById(id)
+    }
+
+    suspend fun findRecentBudget(): Budget {
+        return AppDatabase.instance.budgetDao().findRecentBudget()
+    }
+
+    suspend fun update(budget: Budget) {
+        AppDatabase.instance.budgetDao().update(budget)
+    }
+
+    suspend fun findAll(): List<Budget> {
+        return AppDatabase.instance.budgetDao().findAll()
+    }
+
+    suspend fun findAllDesc(): List<Budget> {
+        return AppDatabase.instance.budgetDao().findAllDesc()
+    }
+
+    suspend fun deleteAllBudget() {
         AppDatabase.instance.budgetDao().deleteAllBudget()
     }
 }
--- app/src/main/java/com/nexters/zzallang/harusal2/data/repository/StatementRepository.kt
+++ app/src/main/java/com/nexters/zzallang/harusal2/data/repository/StatementRepository.kt
@@ -1,47 +1,38 @@
 package com.nexters.zzallang.harusal2.data.repository
 
 import com.nexters.zzallang.harusal2.application.AppDatabase
-import com.nexters.zzallang.harusal2.base.BaseRepository
 import com.nexters.zzallang.harusal2.data.entity.Statement
-import kotlinx.coroutines.withContext
 import java.time.LocalDate
 
-class StatementRepository : BaseRepository() {
+class StatementRepository {
     suspend fun insertStatement(statement: Statement) {
-        withContext(coroutineContext) {
-            AppDatabase.instance.statementDao().insertStatement(statement)
-        }
+        AppDatabase.instance.statementDao().insertStatement(statement)
     }
 
-    suspend fun getStatement(id: Long) =
-        withContext(coroutineContext) {
-            AppDatabase.instance.statementDao().getStatement(id)
-        }
+    suspend fun getStatement(id: Long): Statement {
+        return AppDatabase.instance.statementDao().getStatement(id)
+    }
 
-    suspend fun updateStatement(statement: Statement) =
-        withContext(coroutineContext) {
-            AppDatabase.instance.statementDao().updateStatement(statement)
-        }
+    suspend fun updateStatement(statement: Statement) {
+        AppDatabase.instance.statementDao().updateStatement(statement)
+    }
 
     suspend fun deleteStatement(id: Long) {
-        withContext(coroutineContext) {
-            AppDatabase.instance.statementDao().deleteStatement(id)
-        }
+        AppDatabase.instance.statementDao().deleteStatement(id)
     }
 
     suspend fun deleteAllStatement() {
-        withContext(coroutineContext) {
-            AppDatabase.instance.statementDao().deleteAllStatement()
-        }
+        AppDatabase.instance.statementDao().deleteAllStatement()
     }
 
-    suspend fun selectAllStatementByDate(startTime: LocalDate, endTime: LocalDate) =
-        withContext(coroutineContext) {
-            AppDatabase.instance.statementDao().selectStatementWhereDate(startTime, endTime)
-        }
+    suspend fun selectAllStatementByDate(
+        startTime: LocalDate,
+        endTime: LocalDate,
+    ): List<Statement> {
+        return AppDatabase.instance.statementDao().selectStatementWhereDate(startTime, endTime)
+    }
 
-    suspend fun findStatementByBudgetId(budgetId: Long) =
-        withContext(coroutineContext){
-            AppDatabase.instance.statementDao().findStatementByBudgetId(budgetId)
-        }
+    suspend fun findStatementByBudgetId(budgetId: Long): List<Statement> {
+        return AppDatabase.instance.statementDao().findStatementByBudgetId(budgetId)
+    }
 }
